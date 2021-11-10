85d0bdeeac77bca3db3140320faa8953243510fe
Nouvelle <93885095+Nouvelle-Zelande@users.noreply.github.com>
2021-11-10 18:10:38 +1300

    Original commit for Pika file

---
+++ PikaOrigin.py
@@ -0,0 +1,23 @@
+score = 0
+
+print('Hi!, would you like to play our guessing game?')
+input1 = input()
+if input1 == 'yes':
+    print('Ok lets get started!')
+    Question = input("is pear a fruit or vegetable")
+if Question == "fruit":
+    print ("well done")       
+elif Question == ("vegetable"):
+    print ("aww, it was wrong try again next time.")
+
+yooo = input("is apple a fruit or vegetable")
+if yooo == ("fruit"):
+    print ("well done")
+elif yooo == ("vegetable"):
+    print ("aww, it was wrong try again next time.")
+
+a = input("is onion a fruit or vegetable")
+if a == ("vegetable"):
+    print ("well done")
+elif a == ("fruit"):
+    print ("aww, it was wrong try again next time.")
