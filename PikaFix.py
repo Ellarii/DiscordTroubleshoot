8e92e5ab9e5e2a578c6d31c647625fa865f22de0
Nouvelle <93885095+Nouvelle-Zelande@users.noreply.github.com>
2021-11-10 18:11:04 +1300

    Create PikaFix.py

---
+++ PikaFix.py
@@ -0,0 +1,25 @@
+score = 0
+
+print("Hi!, would you like to play our guessing game?")
+input1 = input()
+if input1 == "yes":
+    print('Ok lets get started!')
+Question = input("is pear a fruit or vegetable")
+if Question == "fruit":
+    print ("well done")
+    score += 1       
+elif Question == ("vegetable"):
+    print ("aww, it was wrong try again next time.")
+
+yooo = input("is apple a fruit or vegetable")
+if yooo == "fruit":
+    print ("well done")
+    score += 1
+elif yooo == ("vegetable"):
+    print ("aww, it was wrong try again next time.")
+a = input("is onion a fruit or vegetable")
+if a == ("vegetable"):
+    print ("well done")
+    score +=1
+elif a == ("fruit"):
+    print ("aww, it was wrong try again next time.")
